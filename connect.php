919e6424f44fa38e9297dc703b8231ae5a2e485a
Dagmawi Teferi <dagiteferi2011@gmail.com>
2024-05-23 05:53:27 -0600

    intal commit

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+// Create connection
+$conn = new mysqli('localhost', 'root', '', 'attsystem');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
